301f0bf6b5de71b16cd14117220ea56b026d9319
Nemo <bick.norales@gmail.com>
2018-12-20 16:48:17 -0800

    test commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,6 +3,22 @@ var data = [
         name:"Ahmed",
         photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         scores:[5,1,4,4,5,1,2,5,4,1]
+    },{
+        name:"Carrie",
+        photo:"../resources/images/carrie.jpg",
+        scores: [3,4,1,5,1,2,2,3,3,5]
+    },{
+        name:"Chris",
+        photo:"../resources/images/christ.jpg",
+        scores:[1,2,3,4,5,4,3,2,1,2]
+    },{
+        name:"Hillary",
+        photo:"../resources/images/hillary.jpg",
+        scores:[2,4,5,5,4,1,4,2,3,1]
+    },{
+        name:"Thomas",
+        photo:"../resources/images/mrT.jpg",
+        scores:[5,4,3,2,1,2,3,2,5,4,2]
     }
 ];
 
